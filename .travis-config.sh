d82a200e87011a573401d1de071c897da4c12cb2
robin <unknown>
2019-12-04 13:44:58 +0000

    fix location

--- .travis-config.sh
+++ .travis-config.sh
@@ -1,19 +1,24 @@
 function pre_build {
-	echo "===== pre_build ls($(pwd))"
+	echo "+++++ pre_build start ls($(pwd))"
 	ls
+	(
 	cd pyfribidi/fribidi-src
 	export PATH="/opt/python/cp38-cp38/bin:$PATH"
 	pip install meson ninja
 	meson -Ddocs=false --backend=ninja build
 	ninja -C build test
+	)
+	echo "+++++ pre_build end"
 	}
 
 function run_tests {
+	echo "+++++ run_tests start ls($(pwd))"
+	ls
 	(
 	echo -n "+++++ python version:";python -c"import sys;print(sys.version.split()[0])"
 	echo -n "+++++ filesystemencoding:";python -c"import sys;print(sys.getfilesystemencoding())"
 	cd ../pyfribidi/src
-	echo "===== in reportlab/tests pwd=`pwd`"
 	python test_pyfribidi.py || true
 	)
+	echo "+++++ run_tests end"
 	}
