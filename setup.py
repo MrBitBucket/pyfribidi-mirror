9fa306c46e163a88395c1916afa72ba0fca68355
robin <unknown>
2023-01-06 09:00:38 +0000

    start on ninja-static branch

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
+import distutils
 try:
     from setuptools import setup, Extension
 except ImportError:
     from distutils.core import setup, Extension
-import sys,os,glob
+import sys,os
 
 pjoin=os.path.join
 normpath = os.path.normpath
@@ -32,7 +33,86 @@ def locationValueError(msg):
     print('!!!!! %s\nls(%r)\n%s\n!!!!!''' % (msg,cwd,spaceListDir(cwd)))
     raise ValueError(msg)
 
+def getFribidiSrc():
+    choices = tuple((
+                normpath(pjoin(d,_)) for d in (pjoin(here,'..'),here)
+                                     for _ in ('fribidi','fribidi-src')
+                ))
+    for d in choices:
+        if isdir(d) and isfile(pjoin(d,'lib','fribidi-common.h')):
+            return d
+    locationValueError('Cannot locate fribidi-src directory from %s' % spaceList(choices))
+
+fribidi_src = getFribidiSrc()
 pyfribidi_src = pjoin(here,'src')
+if verbose:
+    print("+++++ fribidi_src=%r\n+++++ pyfribidi_src=%r" % (fribidi_src,pyfribidi_src))
+
+meson_lib = pjoin(fribidi_src,'build','lib')
+def getIncludeDirs():
+    for _top in ('build',None):
+        top = pjoin(fribidi_src,_top) if _top else fribidi_src
+        lib = pjoin(top,'lib')
+        if isfile(pjoin(top,'config.h')) and isfile(pjoin(lib,'fribidi-config.h')):
+            I = [top,lib]
+            if _top:
+                gen = pjoin(top,'gen.tab')
+                if isfile(pjoin(gen,'fribidi-unicode-version.h')):
+                    I.append(gen)
+                return I
+    locationValueError('''Cannot locate a suitable config.h file.
+    meson -Ddocs=false --backend=ninja build
+    ninja -C build test
+or
+    ./autogen.sh
+    ./configure''')
+include_dirs = getIncludeDirs() + [pjoin(fribidi_src,"lib"),pjoin(fribidi_src,'gen.tab'),pyfribidi_src]
+if verbose:
+    print("+++++ include_dirs=%s" % spaceList(include_dirs))
+
+if isdir(meson_lib):
+    if sys.platform=='win32':
+        if verbose:
+            print('+++++ meson_lib ls(%r)\n%s' % (meson_lib,spaceListDir(meson_lib)))
+        meson_lib = pjoin(meson_lib,'fribidi.lib')
+    else:
+        meson_lib = pjoin(meson_lib,'libfribidi.a')
+
+    if not isfile(meson_lib):
+        meson_lib = None
+else:
+    meson_lib = None
+
+
+libraries = []
+if meson_lib:
+    extra_objects = [meson_lib]
+    lib_sources = []
+    if verbose:
+        print('+++++ using static libraries %s' % spaceList(libraries))
+else:
+    extra_objects = []
+    lib_sources = [pjoin(fribidi_src,p) for p in """
+lib/fribidi.c
+lib/fribidi-arabic.c
+lib/fribidi-bidi.c
+lib/fribidi-bidi-types.c
+lib/fribidi-brackets.c
+lib/fribidi-deprecated.c
+lib/fribidi-joining.c
+lib/fribidi-joining-types.c
+lib/fribidi-mirroring.c
+lib/fribidi-run.c
+lib/fribidi-shape.c
+lib/fribidi-char-sets-cp1256.c
+lib/fribidi-char-sets-iso8859-8.c
+lib/fribidi-char-sets-cap-rtl.c
+lib/fribidi-char-sets-utf8.c
+lib/fribidi-char-sets.c
+lib/fribidi-char-sets-cp1255.c
+lib/fribidi-char-sets-iso8859-6.c
+""".split()]
+
 def get_version():
     d = {}
     try:
@@ -41,46 +121,27 @@ def get_version():
     except (ImportError, RuntimeError):
         pass
     return d["__version__"]
+
 pyFribidiVersion=get_version()
+with open(pjoin(pyfribidi_src,"pyfribidi_version.h"),'w') as f:
+    f.write('#define PYFRIBIDI_VERSION "%s"\n' % pyFribidiVersion)
 
-def include_dirs():
-    n = 0
-    for d in ('/usr/local/include', '/usr/include'):
-        dfi = pjoin(d,'fribidi')
-        if os.path.isdir(dfi):
-            yield dfi
-            n += 1
-    if not n:
-        raise ValueError('could not find includes for fribidi')
-
-def lib_dirs():
-    n = 0
-    for d in ('/usr/local/lib', '/usr/lib'):
-        if glob.glob(pjoin(d,'libfribidi.*')):
-            yield d
-            n += 1
-    if not n:
-        raise ValueError('could not find dynamic library for fribidi')
+define_macros = [("HAVE_CONFIG_H", 1)]
 
 setup(name="pyfribidi",
-    version=pyFribidiVersion,
-    description="Python libfribidi interface",
-    author="Yaacov Zamir, Nir Soffer, Robin Becker",
-    author_email="kzamir@walla.co.il",
-    url="https://github.com/pediapress/pyfribidi",
-    license="GPL",
-    long_description=open("README.rst").read(),
-    package_dir = {'':pyfribidi_src},
-    py_modules=["pyfribidi", "pyfribidi2"],
-    ext_modules=[
-        Extension(
-            name='_pyfribidi',
-            sources=[pjoin(pyfribidi_src,'_pyfribidi.c')],
-            define_macros=[('PYFRIBIDI_VERSION',pyFribidiVersion)],
-            library_dirs = list(lib_dirs()),
-            libraries=['fribidi'],
-            extra_objects = [],
-            include_dirs = list(include_dirs())
-            ),
-        ],
-      )
+  version=pyFribidiVersion,
+  description="Python libfribidi interface",
+  author="Yaacov Zamir, Nir Soffer, Robin Becker",
+  author_email="kzamir@walla.co.il",
+  url="https://github.com/pediapress/pyfribidi",
+  license="GPL",
+  long_description=open("README.rst").read(),
+  package_dir = {'':pyfribidi_src},
+  py_modules=["pyfribidi", "pyfribidi2"],
+  ext_modules=[Extension(
+        name='_pyfribidi',
+        sources=[pjoin(pyfribidi_src,'_pyfribidi.c')] + lib_sources,
+        define_macros=define_macros,
+        libraries=libraries,
+        extra_objects = extra_objects,
+        include_dirs=include_dirs)])
